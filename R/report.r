e40a7cbf39afcb188ad77deb9778575d94efce8c
Aaron Wolen <aaron@wolen.com>
2017-10-17 10:37:59 -0400

    Ability to handle multiple models

--- R/report.r
+++ R/report.r
@@ -3,14 +3,11 @@
 #' @param models a list of `MxModel` objects
 #' @examples
 #' data(AdeFit)
-#' mxReport(models = list(AdeFit))
+#' mxReport(.models = list(AdeFit, AeFit))
 
-mxReport <- function(models, report_file = "openmx_report.html", report_dir = ".", params = report_params(name = "Aaron")) {
+mxReport <- function(.models, report_file = "openmx_report.html", report_dir = ".", params = report_params(name = "Aaron")) {
 
-  stopifnot(all(vapply(models, class, FUN.VALUE = "") == "MxModel"))
-
-  # NOTE: Currently works with a single model
-  model <- models[[1]]
+  stopifnot(all(vapply(.models, class, FUN.VALUE = "") == "MxModel"))
 
   rmd_file <- system.file("openmx_report.Rmd", package = "mxtx")
   report_dir <- normalizePath(report_dir, mustWork = TRUE)
